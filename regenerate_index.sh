05d237fd3ae2f0a1ebb6d95518310f074900768c
Ethan Williams <ethanfwilliams@gmail.com>
2022-06-23 20:11:48 -0700

    updated index

---
+++ regenerate_index.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+out="index.html"
+
+echo > $out << END
+<html>
+<head>
+<title>Ethan Williams's Home Page</title>
+</head>
+
+<body>
+<center><h1>Ethan Williams's Home Page</h1></center>
+
+<table width=80%>
+<tr>
+
+END
+
+echo >> $out <<END
+
+</body>
+</html>
+END
+
