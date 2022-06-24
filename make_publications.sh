9fac556728f8c7c753cb5fa4f703095167a518c2
Ethan Williams <ethanfwilliams@gmail.com>
2022-06-23 20:38:11 -0700

    updated publications

---
+++ make_publications.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+outfile="publications.html"
+
+## Remove old file
+rm $outfile
+
+## Write header
+cat >> $outfile << END
+<html>
+<head>
+<title>Publications</title>
+</head>
+
+<body>
+<center><h1>Publications</h1></center>
+
+<table width=80%>
+<tr>
+
+END
+
+## Add sidebar
+echo "<td valign=top>" >> $outfile
+cat src/sidebar.txt >> $outfile
+echo -e "</td>\n" >> $outfile
+
+## Add body
+echo "<td valign=top width=78%>" >> $outfile
+cat src/publications.txt >> $outfile
+echo -e "</td>\n" >> $outfile
+
+## Add figure
+#<figure style="width:600px;">
+#        <img src="noisePDF_convert.png" width=600 vspace=20>
+#        <figcaption><i>(LEFT)</i> PDF of noise spectra on one channel of Caltech's Pasadena DAS array over three weeks. <i>(RIGHT)</i> Earthquake spectra observed on the same channel, compared with the median noise spectrum and nearby broadband PASC.</figcaption>
+#</figure>
+
+
+## Write end
+cat >> $outfile <<END
+</body>
+</html>
+END
+
